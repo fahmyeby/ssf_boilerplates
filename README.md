1338fbd64f8194c89dca86d996a72547055fe055
fahmyeby <fahmyebyyussoff@yahoo.com.sg>
2024-12-04 10:19:11 +0800

    push

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 "# ssf_boilerplates" 
+RedisConfig class
+Repo - Hash and List
+Application properties for Redis
+Dockerfile
---
+++ src/main/java/com/example/springboot_boilerplates/SpringbootBoilerplatesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_boilerplates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBoilerplatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBoilerplatesApplication.class, args);
+	}
+
+}
